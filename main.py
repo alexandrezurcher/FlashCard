72a394060fe4d1da7a43730168645093ab7f31e0
alexandrezurcher <60798271+alexandrezurcher@users.noreply.github.com>
2022-10-23 21:40:43 +0100

    added flip card function

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ from tkinter import *
 import pandas
 import random
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
 
 data = pandas.read_csv("./data/german_words.csv")
 to_learn = data.to_dict(orient="records")
 def new_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="German")
-    canvas.itemconfig(card_word, text=current_card["German"])
+    canvas.itemconfig(card_title, text="German",fill="black")
+    canvas.itemconfig(card_word, text=current_card["German"],fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English",fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"],fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
 
 
 
@@ -17,9 +28,12 @@ window = Tk()
 window.title("Flashcard Game")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000,flip_card)
+
 canvas = Canvas(width=800, height=526,bg=BACKGROUND_COLOR, highlightthickness=0)
-flash_card_img = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=flash_card_img)
+card_front_img = PhotoImage(file="./images/card_front.png")
+card_back_img = PhotoImage(file="./images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 canvas.grid(column=0, row=0, columnspan=2)
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="word",font=("Ariel", 60, "bold"))
@@ -33,5 +47,8 @@ button_right = Button(image=button_right_img, highlightthickness=0,command=new_c
 button_right.grid(column=1, row=1)
 
 new_card()
+
+
+
 
 window.mainloop()
